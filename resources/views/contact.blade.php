ff435db841d137e259b04df5e1a38f62ec8fa78a
Lucas-GES <lucas.espiritosanto@edu.unifil.br>
2022-05-11 10:35:49 -0300

    first commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+@section('title', 'Teste')
+
+@section('content')
+
+
+<h1>This is a Test</h1>
+<h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad suscipit veritatis saepe laborum illum quia ipsum officia explicabo? Odit voluptatem eligendi sed labore tempore obcaecati ipsum nobis non laboriosam hic.</h4>
+@endsection
